e0c1e326bf7211fa0b619e92414299b6af4981a3
pritishdesai <71164803+pritishdesai@users.noreply.github.com>
2024-06-23 16:46:38 +0530

    Project Created

---
+++ src/test/java/in/pritish/ChallengeApp/ChallengeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.pritish.ChallengeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
